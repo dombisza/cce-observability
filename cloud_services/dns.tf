acc96f8fe96d3c0a201d99de7cfcafc9006d144a
Sener Koban <sener.koban@t-systems.com>
2024-05-14 06:53:31 +0000

    add DNS handling for Grafana FQDN/URL

---
+++ cloud_services/dns.tf
@@ -0,0 +1,27 @@
+variable "GRAFANA_DOMAIN" {
+  type = string
+  description = "export TF_VAR_GRAFANA_DOMAIN='mysubnet'"
+}
+
+variable "GRAFANA_FQDN" {
+  type = string
+  description = "export TF_VAR_GRAFANA_FQDN='mysubnet'"
+}
+
+data "opentelekomcloud_dns_zone_v2" "app_zone" {
+  name = "${var.GRAFANA_DOMAIN}."
+}
+
+resource "opentelekomcloud_dns_recordset_v2" "app_fqdn" {
+  zone_id     = data.opentelekomcloud_dns_zone_v2.app_zone.id
+  name        = "${var.GRAFANA_FQDN}."
+  description = "CCE Grafana access FQDN"
+  ttl         = 300
+  type        = "A"
+  records     = [opentelekomcloud_vpc_eip_v1.elb_eip.publicip[0].ip_address]
+}
+
+output "zone_id" {
+  value = data.opentelekomcloud_dns_zone_v2.app_zone.id
+}
+
