ab779e0bb4efe43c1cf1acc9f4990f170f035a9b
Sener Koban <sener.koban@t-systems.com>
2024-05-14 06:55:34 +0000

    update helmfile + grafana chart + add setting environment variables

--- get_loki_creds.sh
+++ get_loki_creds.sh
@@ -11,4 +11,7 @@ export LOKI_REGION=$(terraform -chdir=./cloud_services show -json | jq -r '.valu
   && echo "Successfully loaded LOKI_REGION" || echo "Failed to load LOKI_REGION"
 export LOKI_S3_EP=https://obs.${LOKI_REGION}.otc.t-systems.com \
   && echo "Successfully loaded LOKI_S3_EP" || echo "Failed to load LOKI_S3_EP"
-
+export GRAFANA_INGRESS_ELB_IP=$(terraform -chdir=./cloud_services show -json | jq -r '.values.outputs.elb_eip.value') \
+  && echo "ELB Public IP GRAFANA_INGRESS_ELB_IP=$GRAFANA_INGRESS_ELB_IP" || echo "Failed to set GRAFANA_INGRESS_ELB_IP"
+export GRAFANA_INGRESS_ELB_ID=$(terraform -chdir=./cloud_services show -json | jq -r '.values.outputs.elb_id.value') \
+  && echo "ELB ID GRAFANA_INGRESS_ELB_ID=$GRAFANA_INGRESS_ELB_ID" || echo "Failed to set GRAFANA_INGRESS_ELB_ID"
